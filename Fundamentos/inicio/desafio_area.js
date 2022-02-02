292a1bfb639370dba8857127249b51ab500e9ed2
Jean Carlos <jean-bra@protonmail.com>
2022-02-02 19:46:13 -0300

    Iniciando com programação - Desafio área da circunferência

---
+++ Fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592;
+const raio = 10;
+let area = PI * raio * raio;
+
+console.log('A área é ' + area + ' m2.');
+
+area = Math.PI * raio * raio;
+console.log('A área é ' + area + ' m2.');
