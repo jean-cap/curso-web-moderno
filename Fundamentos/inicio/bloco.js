d8e490cdf117e60d85aca0c4f3cb911d6968aef5
Jean Carlos <jean-bra@protonmail.com>
2022-02-02 18:43:37 -0300

    Iniciando com programação - Bloco de código

---
+++ Fundamentos/inicio/bloco.js
@@ -0,0 +1,21 @@
+// Este é um bloco de código
+{
+    console.log('Passo #01');
+    console.log('Passo #02');
+    console.log('Passo #03');
+}
+
+// Outro exemplo de bloco de código
+{
+    console.log('Passo #04');
+    console.log('Passo #05');
+}
+
+{
+    {
+        {
+            // Blocos dentro de blocos
+            console.log('Passo #06');
+        }
+    }
+}
