857ad102d87af4422b528bbd1d4dca20a080d4f2
Jean Carlos <jean-bra@protonmail.com>
2022-02-03 16:50:18 -0300

    Javascript - Fundamentos - Usando Math

---
+++ Fundamentos/fundamentos/math.js
@@ -0,0 +1,5 @@
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2); // Math.pow retorna a base ao expoente power.
+
+console.log(area); // 98.5203456165759
+console.log(typeof Math); // object
